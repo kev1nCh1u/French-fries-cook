7a44825f536094a11ee589cb1792f9e127fb054f
kevinyaya <fsd83811@gmail.com>
2018-12-02 15:35:48 +0800

    絕對位置控制

--- RobotControl_ard/RobotControl_ard.ino
+++ RobotControl_ard/RobotControl_ard.ino
@@ -1,13 +1,22 @@
+
 #include "botcon.h"
 
-struct stepmotor{
-const char AIX;
-const int PUL; //define Pulse pin
-const int DIR; //define Direction pin
-const int ENA; //define Enable Pin
-int stp;
+struct stepmotor {
+  const char AIX;
+  const int PUL; //define Pulse pin
+  const int DIR; //define Direction pin
+  const int ENA; //define Enable Pin
+  const int MAXI;
+  int now, stp;
 };
-stepmotor sm[5] = {{'x',A0,A1,38,},{'y',A6,A7,A2,},{'z',46,48,A8,},{'e',26,28,24,},{'t',36,34,30,}};
+stepmotor sm[5] = {
+  {'X', A0, A1, 38, 1000, 0},
+  {'Y', A6, A7, A2, 1000, 0},
+  {'Z', 46, 48, A8, 1000, 0},
+  {'E', 26, 28, 24, 1000, 0},
+  {'T', 36, 34, 30, 1000, 0}
+};
+
 
 void setup() {
   pinMode (sm[0].PUL, OUTPUT);
@@ -25,18 +34,19 @@ void setup() {
   pinMode (sm[4].PUL, OUTPUT);
   pinMode (sm[4].DIR, OUTPUT);
   pinMode (sm[4].ENA, OUTPUT);
-  // initialize serial:  
+  // initialize serial:
   digitalWrite(sm[0].ENA, HIGH);
   digitalWrite(sm[1].ENA, HIGH);
   digitalWrite(sm[2].ENA, HIGH);
   digitalWrite(sm[3].ENA, HIGH);
   digitalWrite(sm[4].ENA, HIGH);
   Serial.begin(115200);
-  Serial.println("enter x y z e t");
+  Serial.println("enter x y z e t\n");
 }
 
 void loop() {
 
+  int i, j;
   // put your main code here, to run repeatedly:
   while (Serial.available() > 0) {
 
@@ -47,9 +57,27 @@ void loop() {
     sm[2].stp = Serial.parseInt();
     sm[3].stp = Serial.parseInt();
     sm[4].stp = Serial.parseInt();
+
     // look for the newlinsm[3]. That's the end of your sentence:
     if (Serial.read() == '\n') {
+      for (i = 0; i < 5; i++) {
+        j = sm[i].stp;
+        sm[i].stp = sm[i].stp - sm[i].now;
+        sm[i].now = j;
+        if (sm[i].stp > sm[i].MAXI) {
+          sm[i].stp = sm[i].MAXI;
+        } else if (sm[i].stp < (sm[i].MAXI * -1)) {
+          sm[i].stp = sm[i].MAXI * -1;
+        }
+      }
       move(sm, 200);
+      Serial.print("now:");
+      for (i = 0; i < 5; i++) {
+        Serial.print(sm[i].AIX);
+        Serial.print(sm[i].now);
+        Serial.print(" ");
+      }
+      Serial.println();
     }
   }
 }
--- RobotControl_ard/botcon.cpp
+++ RobotControl_ard/botcon.cpp
@@ -1,43 +1,45 @@
 #include "botcon.h"
 
-struct stepmotor{
-char AIX;
-int PUL; //define Pulse pin
-int DIR; //define Direction pin
-int ENA; //define Enable Pin
-int stp;
+struct stepmotor {
+  const char AIX;
+  const int PUL; //define Pulse pin
+  const int DIR; //define Direction pin
+  const int ENA; //define Enable Pin
+  const int MAXI;
+  int now, stp;
 };
-int i, j=0;
 
-  
-void move(struct stepmotor sm[], int spe){
 
-      for (i = 0; i < 5; i++){
+
+void move(struct stepmotor sm[], int spe) {
+  int i, j = 0;
+  for (i = 0; i < 5; i++) {
+    /*        Serial.print(sm[i].AIX);
+            Serial.print(sm[i].stp);
+            Serial.println("\t");
+    */
+    if (sm[i].stp >= 0) {
+      digitalWrite(sm[i].DIR, HIGH);
+    } else {
+      digitalWrite(sm[i].DIR, LOW);
+      sm[i].stp *= -1;
+    }
+  }
+  while (!(j)) {
+    j = 1;
+    for (i = 0; i < 5; i++) //Forward 5000 steps
+    {
+      if (sm[i].stp != 0) {
+        digitalWrite(sm[i].PUL, HIGH);
+        delayMicroseconds(spe);
+        digitalWrite(sm[i].PUL, LOW);
+        delayMicroseconds(spe);
+        sm[i].stp --;
+        j = 0;
         Serial.print(sm[i].AIX);
         Serial.print(sm[i].stp);
-        Serial.println("\t");
-
-        if (sm[i].PUL >= 0) {
-          digitalWrite(sm[i].DIR, HIGH);
-        } else {
-          digitalWrite(sm[i].DIR, LOW);
-          sm[i].stp *= -1;
-        }
+        Serial.print(" ");
       }
-      while(!(j)){
-        j = 1;
-        for (i = 0; i < 5; i++) //Forward 5000 steps
-        {
-          if(sm[i].stp != 0){
-            digitalWrite(sm[i].PUL, HIGH);
-            delayMicroseconds(spe);
-            digitalWrite(sm[i].PUL, LOW);
-            delayMicroseconds(spe);
-            sm[i].stp --;
-            j = 0;
-            Serial.print(sm[i].AIX);
-            Serial.println(sm[i].stp);
-          }
-        }
-      }
+    }Serial.println();
+  }
 }
