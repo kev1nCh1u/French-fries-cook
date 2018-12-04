a11a5d43fe314efd05102f7686b74e546715f404
kevinyaya <fsd83811@gmail.com>
2018-12-04 17:46:19 +0800

    main

--- RobotControl_ard/botcon.h
+++ RobotControl_ard/botcon.h
@@ -1,7 +1,7 @@
 #ifndef BOTCON_H
 #define BOTCON_H
 
-#include <Arduino.h>
+#include "Arduino.h"
 #include <SPI.h>
 
 void move(struct stepmotor sm[], int spe);
---
+++ RobotControl_ard/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "Arduino.h"
+#include <SPI.h>
+#include "botcon.h"
+
+#endif
