b6a98b82919cbbaa77147b1ea70d44621e694387
kevinyaya <fsd83811@gmail.com>
2018-12-14 17:39:28 +0800

    robot test

--- AccelStepper/AccelStepper.ino
+++ AccelStepper/AccelStepper.ino
@@ -21,59 +21,31 @@ stepmotor sm[5] = {
 };
 
 
-// Joint 1
-#define E1_STEP_PIN 36
-#define E1_DIR_PIN 34
-#define E1_ENABLE_PIN 30
-
-// Joint 2
-#define Z_STEP_PIN         46
-#define Z_DIR_PIN          48
-#define Z_ENABLE_PIN       62
-#define Z_MIN_PIN          18
-#define Z_MAX_PIN          19
-
-// Joint 3
-#define Y_STEP_PIN         60
-#define Y_DIR_PIN          61
-#define Y_ENABLE_PIN       56
-#define Y_MIN_PIN          14
-#define Y_MAX_PIN          15
-
-// Joint 4
-#define X_STEP_PIN 54
-#define X_DIR_PIN 55
-#define X_ENABLE_PIN 38
-
-// Joint 5
-#define E0_STEP_PIN 26
-#define E0_DIR_PIN 28
-#define E0_ENABLE_PIN 24
-
-
 // EG X-Y position bed driven by 2 steppers
 // Alas its not possible to build an array of these with different pins for each :-(
-AccelStepper joint1(1, E1_STEP_PIN, E1_DIR_PIN);
-AccelStepper joint2(1, Z_STEP_PIN, Z_DIR_PIN);
-AccelStepper joint3(1, Y_STEP_PIN, Y_DIR_PIN);
-AccelStepper joint4(1, X_STEP_PIN, X_DIR_PIN);
-AccelStepper joint5(1, E0_STEP_PIN, E0_DIR_PIN);
+AccelStepper joint1(1, sm[0].PUL, sm[0].DIR);
+AccelStepper joint2(1, sm[1].PUL, sm[1].DIR);
+AccelStepper joint3(1, sm[2].PUL, sm[2].DIR);
+AccelStepper joint4(1, sm[3].PUL, sm[3].DIR);
+AccelStepper joint5(1, sm[4].PUL, sm[4].DIR);
+
+
 
 // Up to 10 steppers can be handled as a group by MultiStepper
 MultiStepper steppers;
 
-//test with uint8 converted to long
-unsigned int x = 1000;
+int i, j, k;
 
 void setup() {
-  Serial.begin(250000);
+  Serial.begin(115200);
 
   // Configure each stepper
-  joint1.setMaxSpeed(1500);
-  joint2.setMaxSpeed(750);
-  joint3.setMaxSpeed(2000);
-  joint4.setMaxSpeed(500);
-  joint5.setMaxSpeed(1000);
+  joint1.setMaxSpeed(200);
+  joint2.setMaxSpeed(200);
+  joint3.setMaxSpeed(200);
+  joint4.setMaxSpeed(200);
+  joint5.setMaxSpeed(200);
+
 
   // Then give them to MultiStepper to manage
   steppers.addStepper(joint1);
@@ -81,17 +53,22 @@ void setup() {
   steppers.addStepper(joint3);
   steppers.addStepper(joint4);
   steppers.addStepper(joint5);
+
+  for (i = 0; i < 5; i++) {
+    pinMode (sm[i].ENA, OUTPUT);
+    digitalWrite(sm[i].ENA, HIGH);
+  }
 }
 
 void loop() {
   long positions[5]; // Array of desired stepper positions
 
   // Back of the envelope calculation for microsteps/revolution, where positions[i] is the number of steps (or microsteps).
-  positions[0] = 0; //4100 microsteps is 1/8 revolutions ----> 32800 microsteps/rev
-  positions[1] = 0; //2000 is 40/360 revolutions ---> 18000 microsteps/rev
-  positions[2] = 0; //4000 is 20/360 revolutions ---> 72000 microsteps/rev
-  positions[3] = 0; //820 is 1/4 revolution (200steps/revolution * 16microsteps/step (since microstepping) ~= 32800 microsteps/rev)
-  positions[4] = 0; //2000 is 50/360 revolution ---> 14400
+  positions[0] = 2000; //4100 microsteps is 1/8 revolutions ----> 32800 microsteps/rev
+  positions[1] = 2000; //2000 is 40/360 revolutions ---> 18000 microsteps/rev
+  positions[2] = 2000; //4000 is 20/360 revolutions ---> 72000 microsteps/rev
+  positions[3] = 2000; //820 is 1/4 revolution (200steps/revolution * 16microsteps/step (since microstepping) ~= 32800 microsteps/rev)
+  positions[4] = 2000; //2000 is 50/360 revolution ---> 14400
 
   steppers.moveTo(positions);
   steppers.runSpeedToPosition(); // Blocks until all are in position
